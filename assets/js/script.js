fc5a16f333299d9ff1f39c654e871891f030ffbb
Paul Haberle <61989020+phaberle@users.noreply.github.com>
2022-04-22 23:05:14 -0500

    apply appropriate CSS class for past, present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,29 @@
 var today = moment(new Date()).format("dddd, MMMM Do");
-var x = moment(new Date()).format("hh");
+var x = moment(new Date()).format("H");
+// console.log(parseInt(x));
 
-console.log(typeof(x));
+// console.log(typeof(x));
 $(document).ready(function() {
     $("#currentDay").text(today);
 });
+
+/*get current hour, convert to Number, get hour data from each row,
+compare equality, apply appropriate class for past, present and future
+*/
+var checkPastPresentFuture = function() {
+    let currHour = parseInt(moment(new Date()).format("H"));
+    $(".row").each(function(i) {
+        let rowHour = $(this).data("hour");
+        if (currHour > rowHour) {
+            $(':nth-child(2)', this).toggleClass("past");
+        }
+        if (currHour == rowHour) {
+            $(':nth-child(2)', this).toggleClass("present");
+        }
+        if (currHour < rowHour) {
+            $(':nth-child(2)', this).toggleClass("future");
+        }
+    })
+}
+
+checkPastPresentFuture();
